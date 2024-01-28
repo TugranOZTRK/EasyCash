574d0a8e4e4bc0237ed006ae0e296a67d1ec35ff
TugranOZTRK <tugranozturk7@gmail.com>
2024-01-29 00:28:17 +0300

    Proje dosyası ekle.

---
+++ BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,28 @@
+﻿using DToLayer.Dtos.AppUserDtos;
+using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator:AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x=>x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez");
+            RuleFor(x=>x.SurName).NotEmpty().WithMessage("Soyad alanı boş geçilemez");
+            RuleFor(x=>x.Username).NotEmpty().WithMessage("Kullanıcı adı alanı boş geçilemez");
+            RuleFor(x=>x.Email).NotEmpty().WithMessage("Email alanı boş geçilemez");
+            RuleFor(x=>x.Password).NotEmpty().WithMessage("Parola alanı boş geçilemez");
+            RuleFor(x=>x.ConfirmPassword).NotEmpty().WithMessage("Parola tekrar alanı boş geçilemez");
+            RuleFor(x=>x.Name).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter girin");
+            RuleFor(x=>x.Name).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girin");
+            RuleFor(x=>x.ConfirmPassword).Equal(y=>y.Password).WithMessage("Parolalarınız eşleşmiyor");
+            RuleFor(x=>x.Email).EmailAddress().WithMessage("Lütfen geçerli bir mail adresi giriniz");
+        }
+    }
+}
---
+++ PresentationLayer/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.Models;
+
+namespace PresentationLayer.Controllers
+{
+    public class LoginController:Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel loginViewModel)
+        {
+            var result = await _signInManager.PasswordSignInAsync(loginViewModel.UserName, loginViewModel.Password,false,true);
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByEmailAsync(loginViewModel.UserName);
+                if (user.EmailConfirmed == true)
+                {
+                    return RedirectToAction("Index", "MyProfile");
+                }
+            }
+            return View();
+        }
+    }
+}
